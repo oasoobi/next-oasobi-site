8f7e7c17c9fe0e1fb4165f47411571d8248c45c1
oasobi <134684352+oasoby@users.noreply.github.com>
2023-12-28 23:41:03 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,25 @@ import Image from "next/image";
 import * as React from "react";
 
 export default function Home() {
-  return (
-    <>
-      <div className="ml-10 pt-60">
-        <Image
-          src="/icon.jpg"
-          alt="icon"
-          width={200}
-          height={200}
-          className="rounded-full"
-        ></Image>
-        <div className="mt-5"
-          <p className="inline text-3xl">/おあそび</p>
-        </div>
-        <div className="mt-10 inline">
-          <p>プログラミングとマイクラが好きな人。</p>
-          <p>Javaとかもやろうと思っている。</p>
-          <p>JavaScript, Python, C#, Go, PHPをやったが初心者レベル。</p>
-        </div>
-      </div>
-      <div></div>
-    </>
-  );
+ return (
+   <>
+     <div className="ml-10 pt-60">
+       <Image
+         src="/icon.jpg"
+         alt="icon"
+         width={200}
+         height={200}
+         className="rounded-full"
+       />
+       <div className="mt-5">
+         <p className="inline text-3xl">/おあそび</p>
+       </div>
+       <div className="mt-10 inline">
+         <p>プログラミングとマイクラが好きな人。</p>
+         <p>Javaとかもやろうと思っている。</p>
+         <p>JavaScript, Python, C#, Go, PHPをやったが初心者レベル。</p>
+       </div>
+     </div>
+   </>
+ );
 }

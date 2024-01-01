d3bad73b7ceb53bd07d0efecc8a31ead654cc57d
oasobi <oasobyy@gmail.com>
2024-01-01 14:54:33 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Metadata } from "next"
 import * as React from "react";
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: "home - oasobi"
@@ -10,8 +11,8 @@ export default function Home() {
   return (
     <>
       <div className="pt-[20%] md:pt-[10%] ml-10 mr-10">
-        <div className="md:flex">
-          <Image src="/icon.jpg" alt="icon" width={175} height={175} className="rounded-full md:ml-8"></Image>
+        <div className="lg:flex">
+          <Image src="/icon.svg" alt="icon" width={175} height={175} className="rounded-full md:ml-8"></Image>
           <div className="mt-[2%] md:ml-[5%]">
             <p className="inline text-3xl">おあそび</p>
             <p>プログラミングとマイクラが好きな人。</p>
@@ -19,24 +20,37 @@ export default function Home() {
           </div>
         </div>
         <div className="md:flex mt-10 md:mt-20 select-none" draggable="false">
-          <div className="boarder border-solid border-[#FF0000] border-[1px] w-[100%] md:w-[25vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer">
-            <div className="flex ml-8 mt-5">
-              <img src="/youtube.svg" width={30} height={30} alt="yt logo" className="dark:invert mr-2 font-bold"/>
-              <p className="text-2xl">Youtube</p>
+          <a href="https://youtube.com/@oasoby">
+            <div className="border border-solid border-[#FF0000] border-[1px] w-[100%] md:w-[20vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer shadow-[0_20px_50px_rgba(255,_0,_0,_0.5)]  transform hover:scale-95 transition-transform duration-300 hover:shadow-none">
+              <div className="flex ml-5 mt-5 items-center">
+                <img src="/youtube.svg" alt="" className="dark:invert mr-2 font-bold w-[30px] h-[30px] md:w-[27px] md:h-[27px]" />
+                <p className="text-[1.2rem] md:text-[1rem]">Youtube</p>
+              </div>
             </div>
-          </div>
-          <div className="border border-solid border-[#1A8CD8] border-[1px] w-[100%] md:w-[25vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer">
-            <div className="flex ml-8 mt-5">
-              <img src="/twitter.svg" width={30} height={30} alt="twitter logo" className="dark:invert mr-2 font-bold"/>
-              <p className="text-2xl">Twitter</p>
+          </a>
+          <a href="https://x.com/oasoby">
+            <div className="border border-solid border-[#1A8CD8] border-[1px] w-[100%] md:w-[20vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer shadow-[0_20px_50px_rgba(8,_112,_184,_0.5)]  transform hover:scale-95 transition-transform duration-300 hover:shadow-none">
+              <div className="flex ml-5 mt-5 items-center">
+                <img src="/twitter.svg" alt="" className="dark:invert mr-2 font-bold w-[30px] h-[30px] md:w-[27px] md:h-[27px]" />
+                <p className="text-[1.2rem] md:text-[1rem]">Twitter</p>
+              </div>
             </div>
-          </div>
-          <div className="border border-solid border-[#000000] border-[1px] dark:border-[#FFFFFF] w-[100%] md:w-[25vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer">
-            <div className="flex ml-8 mt-5">
-              <img src="/github.svg" width={30} height={30} alt="github logo" className="dark:invert mr-2 font-bold"/>
-              <p className="text-2xl">GitHub</p>
+          </a>
+          <a href="https://github.com/oasoby">
+            <div className="border border-solid border-[#000000] border-[1px] dark:border-[#FFFFFF] w-[100%] md:w-[20vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer shadow-[0_15px_45px_rgba(0,_0,_0,_0.5)] dark:shadow-[0_15px_45px_rgba(255,_255,_255,_0.5)]  transform hover:scale-95 transition-transform duration-300 hover:shadow-none">
+              <div className="flex ml-5 mt-5 items-center">
+                <img src="/github.svg" alt="" className="dark:invert mr-2 font-bold w-[30px] h-[30px] md:w-[27px] md:h-[27px]" />
+                <p className="text-[1.2rem] md:text-[1rem]">GitHub</p>
+              </div>
+            </div></a>
+          <Link href="/downloads">
+            <div className="border border-solid border-[#000000] border-[1px] dark:border-[#FFFFFF] w-[100%] md:w-[23vw] h-[20vh] rounded-[20px] mb-10 md:ml-5 md:mr-5 cursor-pointer transform hover:scale-95 transition-transform duration-300">
+              <div className="flex ml-5 mt-5 items-center">
+                <img src="/downloads.svg" alt="" className="dark:invert  mr-2 font-bold w-[30px] h-[30px] md:w-[23px] md:h-[23px]" />
+                <p className="text-[1.2rem] md:text-[1rem]">Downloads</p>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </>
